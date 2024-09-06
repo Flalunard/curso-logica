bd832f301b277e8ce9300c15f36a8f9026a4e637
Flavia kaline <luizarnaldolunardiperroni@gmail.com>
2024-09-06 09:18:19 -0300

    Create exec03.py

---
+++ exec03.py
@@ -0,0 +1,5 @@
+'''- Faça um programa que imprima na tela o nome de uma 
+música que você gosta (proibido letras do Justin Bieber)'''
+
+musica=str
+print("Deus proteja-me da maldade de gente boa e da bondade de gente ruim . " , musica)
