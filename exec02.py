b1926deaa57e09a8c2e2feb55abe6658fb51ab34
Flavia kaline <luizarnaldolunardiperroni@gmail.com>
2024-09-06 09:12:16 -0300

    Create exec02.py

---
+++ exec02.py
@@ -0,0 +1,9 @@
+''' Elabore um programa que imprima seu nome na primeira linha,
+o estado em que você mora na segunda,
+e seu município na terceira.'''
+flavia=str
+joaopessoa=str
+paraiba=str
+print("Olá sou Flavia" ,flavia )
+print("Moro em João Pessoa ", joaopessoa)
+print("Localizda na Paraíba. ", paraiba)
